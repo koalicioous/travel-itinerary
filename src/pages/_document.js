7e0f5c1e47e9f4b181c62d390b64bc9ead2669fe
Surya Negara <surya.negara@gontor.ac.id>
2023-04-09 16:57:11 +0700

    add google script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0W2BZZLJBF"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0W2BZZLJBF');
+        `}
+      </Script>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
